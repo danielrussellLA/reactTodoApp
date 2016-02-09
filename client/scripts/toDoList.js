587113b001eeeb6112e89d7ce51d50c8d96a41f1
Daniel Russell <contactdanielrussell@gmail.com>
2016-02-08 21:15:11 -0800

    Update toDoList.js

--- client/scripts/toDoList.js
+++ client/scripts/toDoList.js
@@ -2,13 +2,13 @@ var React = require('react');
 var ToDoListItem = require('./toDoListItem.js');
 
 var ToDoList = React.createClass({
-	render: function(){
-		return (
-			<ul className="toDoList">
-				<ToDoListItem />
-			</ul>
-		)
-	}
+  render: function(){
+    return (
+      <ul className="toDoList">
+	<ToDoListItem />
+      </ul>
+    )
+  }
 });
 
 module.exports = ToDoList;
