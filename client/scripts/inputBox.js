f19224f29b95ea579f7c7547db12599d82f103ac
Daniel Russell <contactdanielrussell@gmail.com>
2016-02-08 21:12:07 -0800

    fixed spacing

--- client/scripts/inputBox.js
+++ client/scripts/inputBox.js
@@ -1,20 +1,20 @@
 var React = require('react');
 
 var InputBox = React.createClass({
-	handleSubmit: function(){
-		event.preventDefault();
-		this.props.onAddTodoListItem;
-	},
+  handleSubmit: function(){
+    event.preventDefault();
+    this.props.onAddTodoListItem;
+  },
 
-	render: function(){
-		return (
-			<div>
-				<form>
-					<input type='text' onSubmit={this.handleSubmit} />
-				</form>
-			</div>
-		)
-	}
+  render: function(){
+    return (
+      <div>
+        <form>
+	  <input type='text' onSubmit={this.handleSubmit} />
+        </form>
+      </div>
+	   )
+  }
 });
 
 module.exports = InputBox
